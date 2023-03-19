1199f43cc0681c20406cbdb91b4e73765efa2a18
wiltosea <wil@taprapeixe.com.br>
2023-03-19 19:10:12 -0300

    configuracao inicial antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  Button,
+  ConfigProvider,
+  DatePicker,
+  Input,
+  Progress,
+  Slider,
+  Space,
+  Spin,
+} from 'antd';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#1da57a',
+          colorBorder: '#09f',
+          colorText: '#0f9',
+        },
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <Space direction="vertical">
+            <Button type="primary">Button</Button>
+            <Input placeholder="Type here..."></Input>
+            <Progress percent={50} type="circle" />
+            <Spin spinning />
+            <DatePicker
+              onChange={(el) => {
+                console.log(el);
+              }}
+            />
+            <Slider />
+          </Space>
+        </header>
+      </div>
+    </ConfigProvider>
   );
 }
 
